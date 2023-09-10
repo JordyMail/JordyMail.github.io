6f421733e526c24a56814a519819cebbf54c04f3
JordyMail <144602804+JordyMail@users.noreply.github.com>
2023-09-10 14:39:33 +0100

    Add files via upload

---
+++ java/complit.java
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CV Mahasiswa</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <style>
+        /* Reset beberapa default style */
+        body, h1, h2, h3, p, ul, li {
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Atur gaya dasar halaman */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            padding: 20px;
+        }
+
+        header {
+            background-color: #007BFF;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Portofolio Jordy Kastello Mail</h1>
+        <br>
+        <a href="https://drive.google.com/file/d/1iWgmfATdrrKj6axS_iEvetdFMC0Y2Cls/view?usp=drivesdk" target="_blank">Bukti Sertifikat KAMPANYE LITERASI</a>
+        <br>
+        <a href="https://drive.google.com/file/d/1DWlrrn3iza8l3u6DNHUsiL1aBHgdsYfU/view?usp=drivesdk" target="_blank">Bukti Sertifikat CERDAS CERMAT KIMIA</a>
+        <br>
+        <a href="https://drive.google.com/file/d/19m3uaR-q1yKMEJN3e2Lb011GwU7E9J3S/view?usp=drivesdk" target="_blank">Bukti Sertifikat KIHAJAR STEM</a>
+    </header>
+</body>
+</html>
